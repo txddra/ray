9768a4ef40d09a5a9a4c522b069ad586bb7da4cc
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-29 15:00:30 -0400

    pop

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ push: function(value){
 this[this.length++]=value;
 },
 pop:function(){
+    let last = this[this.length - 1]
+    delete this[this.length -1 ]
+    this.length--
+    return last
 
 }
 }
